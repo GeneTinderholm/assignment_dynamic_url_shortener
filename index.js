11c2f83f9291e776f9d8e8b17b3f5a94ed0fb2db
Johann <baptistajohann@gmail.com>
2017-11-21 09:31:54 -0800

    connecting to redis

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const redisClient = require("redis").createClient();
-let shorturl = require('shorturl');
-shorturl('https://google.com', 'arsehat', function(result) {
-	console.log(result);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const redisClient = require('redis').createClient();
+
+var shortUrl = require('node-url-shortener');
+
+shortUrl.short('https://google.com', function(err, url) {
+  console.log(url);
 });
+
 app.use(
-  "/socket.io",
-  express.static(__dirname + "node_modules/socket.io-client/dist/")
+  '/socket.io',
+  express.static(__dirname + 'node_modules/socket.io-client/dist/'),
 );
 
-redisClient.setnx("count", 0);
+redisClient.setnx('count', 0);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", client => {
-  redisClient.get("count", (err, count) => {
-    client.emit("new count", count);
+app.post('/', (req, res) => {
+  const url = req.body.long-url;
+  shortUrl.short(url, (err, shorturl) => {
+    redisClient.setnx(url, shorturl);
+    res.redirect('/');
+  }) 
+})
+
+io.on('connection', client => {
+  redisClient.get('count', (err, count) => {
+    client.emit('new count', count);
   });
 
-  client.on("increment", () => {
-    redisClient.incr("count", (err, count) => {
-      io.emit("new count", count);
+  client.on('increment', () => {
+    redisClient.incr('count', (err, count) => {
+      io.emit('new count', count);
     });
   });
 
-  client.on("decrement", () => {
-    redisClient.decr("count", (err, count) => {
-      io.emit("new count", count);
+  client.on('decrement', () => {
+    redisClient.decr('count', (err, count) => {
+      io.emit('new count', count);
     });
   });
 });
